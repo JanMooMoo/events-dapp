1a66da61375dd41d510e069b31380b5d66aa5f28
JanMooMoo <35936674+JanMooMoo@users.noreply.github.com>
2020-01-28 07:24:35 -0800

    check carousel

--- src/components/FindEvents.jsx
+++ src/components/FindEvents.jsx
@@ -12,7 +12,6 @@ import {Open_events_ABI, Open_events_Address} from '../config/OpenEvents';
 
 // TODO: Make slides dynamic: import slidesJson from '../config/slides.json';
 import topicsJson from '../config/topics.json';
-import Carousels from './Carousels';
 import eventCTAsJson from '../config/event_ctas.json';
 
 
@@ -215,7 +214,48 @@ class FindEvents extends Component
 
 		return(
       <React.Fragment>
-      <Carousels/>
+      <Carousel className="retract-page-inner-wrapper">
+          <Carousel.Item className="slide1">
+            <img className="d-block w-100" src="/images/slides/slide1.png" alt="First slide" />
+            <Carousel.Caption>
+              <h3>Check out a Concert</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <button className="btn btn-dark"><i className="fas fa-ticket-alt"></i> Find Events</button>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item className="slide2">
+          <img className="d-block w-100" src="/images/slides/slide2.png" alt="First slide" />
+            <Carousel.Caption>
+              <h3>Support a Local Charity</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <button className="btn btn-dark"><i className="fas fa-ticket-alt"></i> Find Events</button>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item className="slide3">
+          <img className="d-block w-100" src="/images/slides/slide3.png" alt="First slide" />
+            <Carousel.Caption>
+              <h3>Attend an Exclusive Party</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              <button className="btn btn-dark"><i className="fas fa-ticket-alt"></i> Find Events</button>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item className="slide4">
+          <img className="d-block w-100" src="/images/slides/slide4.png" alt="First slide" />
+            <Carousel.Caption>
+              <h3>Play a New Sport</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              <button className="btn btn-dark"><i className="fas fa-ticket-alt"></i> Find Events</button>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item className="slide5">
+          <img className="d-block w-100" src="/images/slides/slide5.png" alt="First slide" />
+            <Carousel.Caption>
+              <h3>Create Your Own and Sell Tickets</h3>
+              <p>Create your own event, it takes only a minute.</p>
+              <button className="btn btn-dark"><i className="fas fa-ticket-alt"></i> Create Event</button>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
 
 			<div className="retract-page-inner-wrapper-alternative">
 
